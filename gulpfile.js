6acdce23052e968ac9550a90df259d1ff9313c00
Eric Smith <eric@bistrostudios.com>
2016-07-14 00:09:20 -0700

    first draft at form validation js and css

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ elixir(function(mix) {
     .scripts([
         'jquery.min.js',
         'jquery-ui.min.js',
+        'jquery.validate.min.js',
         'slick.js',
         'moment.js',
         'main.js',
