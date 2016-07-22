a0dfc5543ff7a7241cdfe12ccffcd84d792edae8
eric smith <eric.smith@crowdpac.com>
2016-07-22 10:35:41 -0700

    add in shared header file to use in thank you page and index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>REST</title>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport">
-  <meta property="og:site_name" content="REST – Real Escape from the Sex Trade"/>
-  <meta property="og:type" content="website"/>
-  <link href="/css/master.css" rel="stylesheet">
-</head>
-<body>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-29157146-3', 'auto');
-  ga('send', 'pageview');
-</script>
-
-<header>
-  <div class="Logo linked"><a href="/">...</a></div>
-</header>
-
-<div class="GuideGuide"></div>
+@include('_partials/header')
 
 <!-- END HEADER PARTIAL FILE -->
 
--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -1,29 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>REST</title>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport">
-  <meta property="og:site_name" content="REST – Real Escape from the Sex Trade"/>
-  <meta property="og:type" content="website"/>
-  <link href="/css/master.css" rel="stylesheet">
-</head>
-<body>
-<header>
-  <div class="logo linked"><a href="/">...</a></div>
-</header>
+@include('_partials/header')
 
 <!-- END HEADER PARTIAL FILE -->
 
 <section class="FundADay_Intro">
   <div class="row">
     <!-- <div class="BigPhoto Intro"></div> -->
-    <div class="column col-xs-12 col-md-8">
+    <div class="column col-xs-12 col-md-5 col-md-offset-7">
       <article class="CenterBlock">
         <h2 class="Serif">Thank you so much. Your generous gift of ${{$amount}} will provide full funding for {{$date}}, having an inexplicable impact on the women and girls we serve. We've sent a receipt to {{$email}} which you will receive shortly.</h2>
       </article>
     </div>
   </div>
+</section>
+<section class="FundADay_BigPhoto">
 </section>
 
 <script src="/js/all.js"></script>
